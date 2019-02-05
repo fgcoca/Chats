61141adb91f1cda2b5b73118d8f02fddc6f91b1d
Federico Coca <fgcoca6@gmail.com>
2019-02-05 20:15:13 +0100

    Add files via upload

---
+++ 9-2-19-Ponencia-FPGAs-Congreso-Tecnologia-Malaga/Colecciones/Taller-FPGAs/examples/05-Comunicaciones/Escritura/Escritura.ino
@@ -0,0 +1,26 @@
+const int CLK = 12;  //-- Pin de reloj
+const int DAT = 11;  //-- Pin de Datos
+const int CTRL = 10; //-- Pin de control
+
+void setup() {
+  pinMode(CLK, OUTPUT);
+  pinMode(DAT, OUTPUT);
+  pinMode(CTRL, OUTPUT);
+  digitalWrite(CTRL, LOW);
+}
+
+void fpga_write(int value); //implementamos fpga_write
+
+void loop() {
+   fpga_write(0x55);
+   delay(500);
+   fpga_write(0xAA);
+   delay(500);
+}
+
+//Definimos fpga_write
+void fpga_write(int value) {
+ shiftOut(DAT, CLK, MSBFIRST, value);
+ digitalWrite(CTRL, HIGH);
+ digitalWrite(CTRL, LOW);
+}
